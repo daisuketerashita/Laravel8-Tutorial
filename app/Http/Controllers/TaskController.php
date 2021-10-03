f49aae6f6efbcd50aa80fdb09a967db0f5a5e606
daisuketerashita <ekusiad.tera@icloud.com>
2021-10-03 18:27:16 +0900

    Taskコントローラの作成と一覧ページ遷移の設定

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //一覧画面の表示
+    public function index(){
+        return 'Hello World';
+    }
+}
