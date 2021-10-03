17f764aae514a1b1b33e303133cd9c06a8076bb3
daisuketerashita <ekusiad.tera@icloud.com>
2021-10-03 19:16:59 +0900

    tasksテーブルとTaskモデルの作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,9 +8,12 @@ use App\Models\Folder;
 class TaskController extends Controller
 {
     //一覧画面の表示
-    public function index(){
+    public function index(int $id){
         $folders = Folder::all();
 
-        return view('tasks.index',['folders' => $folders]);
+        return view('tasks.index',[
+            'folders' => $folders,
+            'current_folder_id' => $id,
+        ]);
     }
 }
