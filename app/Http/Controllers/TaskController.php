47f6c58988f9db22fc82f3fb502e7f2beff7967e
daisuketerashita <ekusiad.tera@icloud.com>
2021-10-05 22:07:33 +0900

    タスク作成画面の作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -25,4 +25,9 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
+    //タスク作成画面の表示
+    public function showCreateForm(int $id){
+        return view('tasks.create',['folder_id' => $id]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/folders/{id}/tasks/',[TaskController::class,'index'])->name('tasks.
 //フォルダ作成
 Route::get('/folders/create/',[FolderController::class,'showCreateForm'])->name('folders.create');
 Route::post('/folders/create/',[FolderController::class,'create']);
+
+//タスク作成
+Route::get('/folders/{id}/tasks/create',[TaskController::class,'showCreateForm'])->name('tasks.create');
+Route::post('/folders/{id}/tasks/create',[TaskController::class,'create']);
